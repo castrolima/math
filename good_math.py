f4edf68bed1033ec61a661aed122d629dfc7e688
Igor Lima <castrolima@gmail.com>
2020-04-11 20:23:21 -0300

    Mudando a divisao de 3 para 4

--- good_math.py
+++ good_math.py
@@ -5,7 +5,7 @@ class CalculadoraGeometria:
     @classmethod
     def volume_piramide(cls, lado_base, altura):
             area_base = (lado_base**2*sqrt(3))/4
-            return(area_base*altura)/3
+            return(area_base*altura)/4
 
 def main ():
     print(CalculadoraGeometria.volume_piramide(lado_base=3,altura=4))
